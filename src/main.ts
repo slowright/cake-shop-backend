1570dcbd41f55fb8a1de19d59bd1152d2f15f4d1
Nikita <vfhbz94@bk.ru>
2023-05-11 22:19:59 +0300

    .

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   await app.listen(3000, () => {
-    console.log('Server has been started!!!');
+    console.log('Server has been started');
   });
 }
 bootstrap();
