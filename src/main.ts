b43c63e08e4e869039623275893afa48cfb1badd
Nikita <vfhbz94@bk.ru>
2023-05-17 23:32:28 +0300

    origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    allowedHeaders: 'Content-type',
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   await app.listen(3000, () => {
