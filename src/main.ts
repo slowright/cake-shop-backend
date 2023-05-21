0ec29b2c37d98f4b45f8a1e8f92f6dbe559e25a8
Nikita <vfhbz94@bk.ru>
2023-05-21 16:15:10 +0300

    +header

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'localhost:3000',
     credentials: true,
     allowedHeaders: ['Authorization', 'Content-type'],
     methods: ['POST', 'GET', 'PUT', 'PATCH', 'DELETE'],
